8b71281b386bfa74d797157209505fa00a7f4cef
akshay2009 <saxe.akshay2009@gmail.com>
2024-05-29 20:05:47 +0530

    Updated admin tools api

--- app/routes/adminTools.routes.js
+++ app/routes/adminTools.routes.js
@@ -12,23 +12,37 @@ module.exports = function (app) {
     uploadadmintoolsdetails.single("file"), // Adjusted to use `single` instead of `fields`
     controller.uploadXLSX
   );
+  /**
+   * Endpoint to Get adminTools details--
+   * @param {String} '/api/admin/tools/",' - API endpoint path.
+   * @param {Function} controller.getAll, - Controller function to get admin tools details--**/
 
   app.get(
     "/api/admin/tools",
     //[authJwt.verifyToken],
     controller.getAll
   );
+  /**
+   * Endpoint to update adminTools details--
+   * @param {String} '/api/admin/tools/:id",' - API endpoint path.
+   * @param {Function} controller.update, - Controller function to update adminTools details--**/
 
+  app.put("/api/admin/tools/:id", [authJwt.verifyToken], controller.update);
+  /**
+   * Endpoint to delete adminTools details--
+   * @param {String} '/api/admin/tools/:id",' - API endpoint path.
+   * @param {Function} controller.delete, - Controller function to delete admin tools details--**/
 
-  app.put(
-    "/api/admin/tools/:id",
+  app.delete("/api/admin/tools/:id", [authJwt.verifyToken], controller.delete);
+
+  /**
+   * Endpoint to Get By search adminTools details--
+   * @param {String} '/api/admin/tools/search/:fieldName/:fieldValue",' - API endpoint path.
+   * @param {Function} controller.search, - Controller function to get admin tools details--**/
+
+  app.get(
+    "/api/admin/tools/search/:fieldName/:fieldValue",
     [authJwt.verifyToken],
-    controller.update
-  );
-
-  app.delete(
-    "/api/admin/tools/:id",
-    [authJwt.verifyToken],
-    controller.delete
+    controller.search
   );
 };
